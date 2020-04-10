91792c5361d993008fda0487470c0dd5a4f890e9
Pankaj P <54427553+PankHub@users.noreply.github.com>
2020-04-10 18:32:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LILER
+
+
+A simple url shortener application that is created using the API of [RELINK URL SHORTENER](https://rel.ink/) (which already is a great application)
+
+## WHY WAS LILER CREATED?
+
+There are many url shorteners available online. This application was made to learn few development and branding concepts. This project will be part of series of small applications created using various new front-end and back-end technologies and system designs. And these applications will be used to practise and learns new test automation tools and frameworks.
+
+## SCREENSHOT
+
+![LILER](img/liler.png)
---
+++ app/app.js
@@ -0,0 +1,32 @@
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+$(document).ready(function () {
+    $("#shorten").click(function () {
+        $("#section").empty();
+        const url = $("#input").val();
+        $.post("https://rel.ink/api/links/",
+            {
+                url: url
+            },
+            function (data, status) {
+                const shortUrl = 'https://rel.ink/' + data.hashid;
+                $("#section").append(`<p>Your shortened URL is &rarr;  <a id="shorturl" target='_blank' href='${shortUrl}'>${shortUrl}</a></p>`);
+                $("#section").append(`&nbsp; <a href="#"><img onclick="copyToClipboard('#shorturl');" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABOklEQVQ4jWNkwANSZl+axMrKxIlL/sP33wLLMw1DkcUY8RnYsfX+/XBreQVc8m3rrv//8/vv1fnperowMSZ8BhIC8mK8jNb68trxMy9fhbswZdaF6dqyvB7YNPxnYJB5/v4XCwcbM0OSiwqG/NIjTxiMNGQYnr35/P/wxUfXF6brarNwsDJxFngo4fTWgkNPGGTEeBlm7brNkOamiiL37O0XBubbzxgYGBgYOdmZNcMnn13PQozXVCT5GBgYGDAMLfXXQFIlxdi+5spHosNQRZKPwUlXnGHWrtt41RF0oZ4sL8Ouc4/h/D9//1JmoJEiP4ORIj+cv+DQE7zqKUo2owaOVAOJynrI4OvP33/WHn/0AJvczz//vpNsIBMD45MSTwVV3PJUBizff/3dkL3gEgexGn7++fcDnzwA5+tg+adEK0gAAAAASUVORK5CYII="></a>`);
+            });
+    });
+
+    $("#expand").click(function () {
+        $("#section").empty();
+        const id = $("#input").val();
+        $.get("https://rel.ink/api/links/" + id, function (data, status) {
+            const expandedUrl = data.url;
+            $("#section").append(`<p>Your expanded URL is &rarr;  <a id="expandedurl" target='_blank' href='${expandedUrl}'>${expandedUrl}</a></p>`);
+        });
+    });
+
+});
